cca70541ee9a092eeb8d3fe96d4edcad06cda8dd
vzuevskiy <vzuevskiy@gmail.com>
2020-12-11 03:15:50 +0700

    Добавлены действия при нажатии кнопок: вывод текста в лог

--- src/main/java/com/example/hw1_skillbox_android_dev_course/MainActivity.kt
+++ src/main/java/com/example/hw1_skillbox_android_dev_course/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.hw1_skillbox_android_dev_course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val b1 = findViewById<Button>(R.id.button1)
+        b1.setOnClickListener {
+            Log.e("!@#", "Button One was pressed!")
+        }
+        val b2 = findViewById<Button>(R.id.button2)
+        b2.setOnClickListener {
+            Log.e("!@#", "Button Two was pressed!")
+        }
+        val b3 = findViewById<Button>(R.id.button3)
+        b3.setOnClickListener {
+            Log.e("!@#", "Button Three was pressed!")
+        }
     }
 }
