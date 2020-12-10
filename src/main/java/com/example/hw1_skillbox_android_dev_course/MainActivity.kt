1b55e4d02c5929a92732b94bc8d58a3d7a029e04
vzuevskiy <vzuevskiy@gmail.com>
2020-12-11 01:18:05 +0700

    initial commit

---
+++ src/main/java/com/example/hw1_skillbox_android_dev_course/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hw1_skillbox_android_dev_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
